3d3d24c3fe3d95d25a5f06c3b8cdc33959fdcead
Sxtanna <ranald.taylor@gmail.com>
2021-11-03 07:01:22 -0400

    feat: added remote database interface

---
+++ element-remote/src/main/java/com/sxtanna/mc/element/remote/RemoteDatabase.java
@@ -0,0 +1,29 @@
+package com.sxtanna.mc.element.remote;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.sxtanna.mc.element.common.state.State;
+import com.sxtanna.mc.element.future.Async;
+import com.sxtanna.mc.element.result.handling.Result;
+
+import java.util.concurrent.CompletableFuture;
+
+public interface RemoteDatabase<C> extends State
+{
+
+    @Override
+    void load();
+
+    @Override
+    void kill();
+
+
+    @NotNull Result<C> connect();
+
+
+    default @NotNull CompletableFuture<C> connectAsync()
+    {
+        return Async.get(this::connect).thenApply(it -> it.asSuccess().some());
+    }
+
+}
