e08e25e1c45693b6f9cbd185f2b7cd80c619e3b8
Sxtanna <ranald.taylor@gmail.com>
2021-11-04 08:04:30 -0400

    feat: added element module for variable binding creation

---
+++ element-system/src/main/java/com/sxtanna/mc/element/system/ElementModule.java
@@ -0,0 +1,10 @@
+package com.sxtanna.mc.element.system;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface ElementModule
+{
+
+    void setup(@NotNull final ElementSystem system);
+
+}
